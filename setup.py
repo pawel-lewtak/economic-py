24b4eeef41183542f40e483e2754edad0b4c523f
pawel <pawel.lewtak@xstream.net>
2015-03-19 14:57:10 +0100

    Add coveralls support

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
+from setuptools.command.test import test as TestCommand
 import setuptools
+import sys
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setuptools.setup(
     name="economic-py",
@@ -12,6 +35,7 @@ setuptools.setup(
 
     packages=setuptools.find_packages(),
 
+    tests_require=['pytest'],
     install_requires=[
         "gdata==2.0.18",
         "requests==2.6.0",
@@ -19,6 +43,7 @@ setuptools.setup(
         "responses==0.3.0",
         "google-api-python-client==1.4.0"
     ],
+    cmdclass={'test': PyTest},
 
     classifiers=[
         'Development Status :: 4 - Beta',
